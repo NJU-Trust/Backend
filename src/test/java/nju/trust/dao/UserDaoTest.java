213b934ee2abcc09c17c3dae38772edc7ca90aa2
Lim <879414458@qq.com>
2018-08-13 21:17:51 +0800

    just test

--- src/test/java/nju/trust/dao/UserDaoTest.java
+++ src/test/java/nju/trust/dao/UserDaoTest.java
@@ -1,10 +1,10 @@
 package nju.trust.dao;
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
 // @RunWith(SpringRunner.class)
 // @DataJpaTest
