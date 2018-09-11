c25f31799867c9a01bc910eadc740ddc33581356
161250127 <361487214@qq.com>
2018-09-11 21:48:25 +0800

    校园验证

--- src/main/java/nju/trust/payloads/verifyInfo/SchoolVerifyInfo.java
+++ src/main/java/nju/trust/payloads/verifyInfo/SchoolVerifyInfo.java
@@ -29,6 +29,83 @@ public class SchoolVerifyInfo {
     //校园卡照片路径
     private String schoolCardImage;
 
+    public String getRealName() {
+        return realName;
+    }
 
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
 
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getBrithday() {
+        return brithday;
+    }
+
+    public void setBrithday(String brithday) {
+        this.brithday = brithday;
+    }
+
+    public String getIdCardNumber() {
+        return idCardNumber;
+    }
+
+    public void setIdCardNumber(String idCardNumber) {
+        this.idCardNumber = idCardNumber;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getAlipay() {
+        return alipay;
+    }
+
+    public void setAlipay(String alipay) {
+        this.alipay = alipay;
+    }
+
+    public String getStuCardImage() {
+        return stuCardImage;
+    }
+
+    public void setStuCardImage(String stuCardImage) {
+        this.stuCardImage = stuCardImage;
+    }
+
+    public String getSchoolCardImage() {
+        return schoolCardImage;
+    }
+
+    public void setSchoolCardImage(String schoolCardImage) {
+        this.schoolCardImage = schoolCardImage;
+    }
 }
---
+++ src/main/java/nju/trust/service/verify/VerifyServiceImpl.java
@@ -0,0 +1,16 @@
+package nju.trust.service.verify;
+
+import nju.trust.payloads.ApiResponse;
+import nju.trust.payloads.verifyInfo.SchoolVerifyInfo;
+
+/**
+ * @Author: 161250127
+ * @Description:
+ * @Date: 2018/9/11
+ */
+public class VerifyServiceImpl implements VerifyService {
+    @Override
+    public ApiResponse schoolVerify(SchoolVerifyInfo schoolVerifyInfo) {
+        return null;
+    }
+}
