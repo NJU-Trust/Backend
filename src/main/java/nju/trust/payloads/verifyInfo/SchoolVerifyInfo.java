54950a4b06040b4d302c0471084bfbe45e8e1c78
161250127 <361487214@qq.com>
2018-09-11 21:46:48 +0800

    校园验证

---
+++ src/main/java/nju/trust/payloads/verifyInfo/SchoolVerifyInfo.java
@@ -0,0 +1,34 @@
+package nju.trust.payloads.verifyInfo;
+
+import nju.trust.entity.user.Gender;
+
+/**
+ * @Author: 161250127
+ * @Description:
+ * @Date: 2018/9/11
+ */
+public class SchoolVerifyInfo {
+    //姓名
+    private String realName;
+    //性别
+    private Gender gender;
+    //出生日期
+    private String brithday;
+    //身份证号
+    private String idCardNumber;
+    //学校
+    private String university;
+    //学院
+    private String institution;
+    //专业
+    private String major;
+    //
+    private String alipay;
+    //学生证照片
+    private String stuCardImage;
+    //校园卡照片路径
+    private String schoolCardImage;
+
+
+
+}
