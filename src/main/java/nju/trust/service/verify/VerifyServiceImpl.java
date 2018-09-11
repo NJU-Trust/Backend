351f29bc7b8575d1b09098c089a5ef97e333e063
161250127 <361487214@qq.com>
2018-09-11 21:53:56 +0800

    校园验证

--- src/main/java/nju/trust/service/verify/VerifyServiceImpl.java
+++ src/main/java/nju/trust/service/verify/VerifyServiceImpl.java
@@ -2,12 +2,14 @@ package nju.trust.service.verify;
 
 import nju.trust.payloads.ApiResponse;
 import nju.trust.payloads.verifyInfo.SchoolVerifyInfo;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author: 161250127
  * @Description:
  * @Date: 2018/9/11
  */
+@Service
 public class VerifyServiceImpl implements VerifyService {
     @Override
     public ApiResponse schoolVerify(SchoolVerifyInfo schoolVerifyInfo) {
