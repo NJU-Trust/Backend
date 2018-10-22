e28c22aec15af1f61779be1b904b6959d17c1f59
161250127 <361487214@qq.com>
2018-10-22 09:42:40 +0800

    complete cross check random choose

--- src/main/java/nju/trust/service/verify/CrossCheckServiceImpl.java
+++ src/main/java/nju/trust/service/verify/CrossCheckServiceImpl.java
@@ -2,11 +2,14 @@ package nju.trust.service.verify;
 
 import nju.trust.dao.user.UserCrossCheckRepository;
 import nju.trust.dao.user.UserRepository;
+import nju.trust.entity.user.CreditCrossCheck;
 import nju.trust.entity.user.User;
 import nju.trust.payloads.ApiResponse;
 import nju.trust.payloads.verifyInfo.CrossCheckInfo;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,9 +37,28 @@ public class CrossCheckServiceImpl implements CrossCheckService {
         if(userRepository.existsByStudentId(studentId1)&&userRepository.existsByStudentId(studentId2)&&userRepository.existsByStudentId(studentId3)){
             //1.find users having the same institution
             User user = userRepository.findByUsername(username).get();
-            Optional<User> users = userRepository.findByInstitution(user.getInstitution());
+            List<User> users = userRepository.findByInstitution(user.getInstitution());
             //2.find random 7 person and set up network
-            
+            if(users.size()<11){
+                return new ApiResponse(false,"the users are not enough to verify!");
+            }else{
+                LocalDate now = LocalDate.now();
+                LocalDate endDate = now.plusDays(3);
+                //choose 3
+                userCrossCheckRepository.save(new CreditCrossCheck(user, userRepository.findByStudentId(studentId1).get(), endDate, false));
+                userCrossCheckRepository.save(new CreditCrossCheck(user, userRepository.findByStudentId(studentId2).get(), endDate, false));
+                userCrossCheckRepository.save(new CreditCrossCheck(user, userRepository.findByStudentId(studentId3).get(), endDate, false));
+                //random 7
+                Collections.shuffle(users);
+                for(int i=0;i<=11;i++){
+                    if(users.get(i).getUsername().equals(username)||users.get(i).getStudentId().equals(studentId1)
+                            ||users.get(i).getStudentId().equals(studentId2)||users.get(i).getStudentId().equals(studentId3)){
+                        continue;
+                    }else{
+                        userCrossCheckRepository.save(new CreditCrossCheck(user, users.get(i), endDate, true));
+                    }
+                }
+            }
         }else {
             return new ApiResponse(false,"studentId not exist!");
         }
