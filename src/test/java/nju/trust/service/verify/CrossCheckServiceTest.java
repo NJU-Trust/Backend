1fd10f17f170a4a683913275df04cc2ff9e94d98
161250127 <361487214@qq.com>
2018-10-23 10:18:44 +0800

    测试交叉检验得到待填问卷列表 测试提交填写结果success

--- src/test/java/nju/trust/service/verify/CrossCheckServiceTest.java
+++ src/test/java/nju/trust/service/verify/CrossCheckServiceTest.java
@@ -1,11 +1,14 @@
 package nju.trust.service.verify;
 
 import nju.trust.payloads.ApiResponse;
+import nju.trust.payloads.verifyInfo.CrossCheckInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -22,17 +25,40 @@ public class CrossCheckServiceTest {
     private CrossCheckService crossCheckService;
 
     @Test
-    public void setUpNetwork() {
+    public void setUpNetwork1() {
         ApiResponse apiResponse = crossCheckService.setUpNetwork("cross1","161250002","161250003","161250004");
         System.out.println(apiResponse.getSuccess());
         System.out.println(apiResponse.getMessage());
     }
 
     @Test
-    public void getQuestionnaireList() {
+    public void setUpNetwork2() {
+        ApiResponse apiResponse = crossCheckService.setUpNetwork("cross2","161250001","161250003","161250004");
+        System.out.println(apiResponse.getSuccess());
+        System.out.println(apiResponse.getMessage());
     }
 
     @Test
-    public void submitQuestionnaire() {
+    public void getQuestionnaireList() {
+        List<CrossCheckInfo> list = crossCheckService.getQuestionnaireList("cross2");
+        for(int i=0;i<list.size();i++){
+            System.out.println(i+":");
+            System.out.println("id:"+list.get(i).getId());
+            System.out.println("person stu_id:"+list.get(i).getStudentId());
+        }
     }
+
+    @Test
+    public void submitQuestionnaire1() {
+        crossCheckService.submitQuestionnaire(1,1,1,1,1,1,1,1,9,9);
+        crossCheckService.submitQuestionnaire(2,4,4,4,3,4,4,1,5,9);
+    }
+
+    @Test
+    public void submitQuestionnaire2() {
+        crossCheckService.submitQuestionnaire(3,1,1,1,1,1,1,1,9,9);
+        crossCheckService.submitQuestionnaire(4,4,4,4,3,4,4,1,5,9);
+        crossCheckService.submitQuestionnaire(5,2,2,2,2,2,2,1,8,2);
+    }
+
 }
