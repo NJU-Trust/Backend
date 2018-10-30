8e69c2010c70fb13b5891c480ae47723b49b7c42
161250173 <479026126@qq.com>
2018-10-31 07:39:17 +0800

    debug:targetadminbriefinfo;修改pendingtargetdetail中枚举类为string

--- src/main/java/nju/trust/entity/CheckState.java
+++ src/main/java/nju/trust/entity/CheckState.java
@@ -9,20 +9,30 @@ public enum CheckState {
     /**
      * Check is on going
      */
-    ONGING,
+    ONGING("审核中"),
 
     /**
      * The application has been approved
      */
-    PASS,
+    PASS("通过"),
 
     /**
      * The application was rejected
      */
-    REJECT,
+    REJECT("未通过"),
 
     /**
      * 用户在不通过后更新信息
      */
-    UPDATE
+    UPDATE("已更新信息");
+
+    private final String str;
+
+    CheckState(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
 }
